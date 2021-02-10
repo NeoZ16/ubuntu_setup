02e4129fb6fd49afab9cc4f7259fb969b7f440dc
Morten Harter <mortenharter@gmail.com>
2021-02-10 18:38:02 +0100

    Fixed some errors and a dependency issue

--- scripts/zz-zsh_setup.bash
+++ scripts/zz-zsh_setup.bash
@@ -3,5 +3,5 @@
 #SUMMARY Install and configure zsh and oh my zsh with custom theme
 sudo apt -y install zsh
 export RUNZSH=no
-yes | sudo sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+yes | sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 ./omz_setup.bash
